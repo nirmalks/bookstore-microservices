37845b207d866c187ede855f72fd9c57821af800
nirmalks <kagosekar@gmail.com>
2025-10-10 15:49:14 +0530

    Add config server properties and remove properties from individual services

--- config-server/src/main/java/com/nirmalks/bookstore_config/ConfigServerApplication.java
+++ config-server/src/main/java/com/nirmalks/bookstore_config/ConfigServerApplication.java
@@ -2,14 +2,16 @@ package com.nirmalks.bookstore_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class BookstoreConfigApplication {
+@EnableDiscoveryClient
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookstoreConfigApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
